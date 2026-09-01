34cbe38572141b8f6d008b35bec3dad19d628011
mega-lindsay <mega-lindsay@gomega.ai>
2026-09-01 23:39:17 +0000

    Add blog post: Chicken Coop Storage Ideas

--- src/data/blog-posts.ts
+++ src/data/blog-posts.ts
@@ -1,5 +1,6 @@
 import threeDShedConfigurator from "./three-d-shed-configurator";
 import chickenCoopGuide from "./chicken-coop-guide";
+import chickenCoopStorageIdeas from "./chicken-coop-storage-ideas";
 import rentToOwnShedsNoCreditCheck from "./rent-to-own-sheds-no-credit-check";
 import automatedChickenTractorGuide from "./automated-chicken-tractor-guide";
 import backyardBarnIdeas from "./backyard-barn-ideas";
@@ -31,6 +32,7 @@ const blogPosts: BlogPost[] = [
 
   rentToOwnShedsNoCreditCheck,
   chickenCoopGuide,
+  chickenCoopStorageIdeas,
   automatedChickenTractorGuide,
   backyardBarnIdeas,
   backyardGreenhouseForSale,
